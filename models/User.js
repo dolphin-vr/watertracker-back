1824f44f4b1e0cb4f8247884142b1c47179fdb3a
dolphin <fun53@ukr.net>
2023-12-24 12:52:59 +0200

    trymer-2

--- models/User.js
+++ models/User.js
@@ -7,38 +7,37 @@ const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 const genderList = ["woman", "man"];
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      minLength: 3,
-      required: [true, "Choose your username"],
-    },
-    password: {
-      type: String,
-      required: [true, "Set password for user"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      match: emailRegexp,
-      unique: true,
-    },
-    gender: {
-      type: String,
-      enum: ["woman", "man"],
-    },
-    avatarURL: String,
-    token: {
-      type: String,
-      default: null,
-    },
-    waterNorma: { type: Number, min: 0, max: 15000, default: 0 },
-    verify: {
-      type: String,
-      default: "",
-    },
-  },
-  { versionKey: false, timestamps: true }
+   {
+      username: {
+         type: String,
+         minLength: 3,
+      },
+      password: {
+         type: String,
+         required: [true, "Set password for user"],
+      },
+      email: {
+         type: String,
+         required: [true, "Email is required"],
+         match: emailRegexp,
+         unique: true,
+      },
+      gender: {
+         type: String,
+         enum: ["woman", "man"],
+      },
+      avatarURL: String,
+      token: {
+         type: String,
+         default: null,
+      },
+      waterNorma: { type: Number, min: 0, max: 15000, default: 0 },
+      verify: {
+         type: String,
+         default: "",
+      },
+   },
+   { versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", handleSaveError);
@@ -59,37 +58,37 @@ export const SignUpSchema = Joi.object({
 });
 
 export const SignInSchema = Joi.object({
-  email: Joi.string().required().pattern(emailRegexp).messages({
-    "any.required": "missing required field 'email'",
-    "string.pattern.base": "'email' must be valid e-mail",
-  }),
-  password: Joi.string().required().min(8).max(48).messages({
-    "any.required": "missing required field 'password'",
-    "string.base": "'password' must be string",
-  }),
+   email: Joi.string().required().pattern(emailRegexp).messages({
+      "any.required": "missing required field 'email'",
+      "string.pattern.base": "'email' must be valid e-mail",
+   }),
+   password: Joi.string().required().min(8).max(48).messages({
+      "any.required": "missing required field 'password'",
+      "string.base": "'password' must be string",
+   }),
 });
 
 export const VerifySchema = Joi.object({
-  email: Joi.string().required().pattern(emailRegexp).messages({
-    "any.required": "missing required field 'email'",
-    "string.pattern.base": "'email' must be valid e-mail",
-  }),
+   email: Joi.string().required().pattern(emailRegexp).messages({
+      "any.required": "missing required field 'email'",
+      "string.pattern.base": "'email' must be valid e-mail",
+   }),
 });
 
 export const UpdateUserInfoSchema = Joi.object({
-  username: Joi.string().min(3).messages({
-    "string.base": "'username' must be string",
-  }),
-  email: Joi.string().pattern(emailRegexp).messages({
-    "string.pattern.base": "'email' must be valid e-mail",
-  }),
-  password: Joi.string().min(8).max(48).messages({
-    "string.base": "'password' must be string",
-  }),
-  newpassword: Joi.string().min(8).max(48).messages({
-    "string.base": "'password' must be string",
-  }),
-  gender: Joi.string().valid(...genderList),
+   username: Joi.string().min(3).messages({
+      "string.base": "'username' must be string",
+   }),
+   email: Joi.string().pattern(emailRegexp).messages({
+      "string.pattern.base": "'email' must be valid e-mail",
+   }),
+   password: Joi.string().min(8).max(48).messages({
+      "string.base": "'password' must be string",
+   }),
+   newpassword: Joi.string().min(8).max(48).messages({
+      "string.base": "'password' must be string",
+   }),
+   gender: Joi.string().valid(...genderList),
 });
 
 export default User;
