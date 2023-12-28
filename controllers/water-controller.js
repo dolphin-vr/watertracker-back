047890e0b953541d867bdcab9124b71ef8bf102b
dolphin <fun53@ukr.net>
2023-12-28 17:34:00 +0200

    today2

--- controllers/water-controller.js
+++ controllers/water-controller.js
@@ -37,11 +37,32 @@ const deleteDoze = async (req, res, next)=>{
 
 const getDaily = async (req, res)=>{
    const {_id: user} = req.user;
+   const norma = req.user.waterNorma / 100 || 1;
    const date = req.params.date;
-   const query = {user, date};
-   const result = await Water.find(query, "-date -user -createdAt -updatedAt");
-   res.json({date, dailyPortions: result});
+   // const query = {user, date};
+   // const result = await Water.find(query, "-date -user -createdAt -updatedAt");
+   const query = [
+      { $match: {"date": date} },
+      { $sort: {time: 1} },
+      { $group: {
+         _id: "$date", 
+         dailyPortions: {$push: {id: "$_id", time: "$time", water: "$water"}}, 
+         daily: {$sum: "$water"}, 
+         doses: {$count: {}}}
+      },
+      { $replaceWith: { date: "$_id", percentage: { $round : { $divide: [ "$daily", norma]}}, doses: "$doses", dailyPortions: "$dailyPortions" } },
+   ];
+   const result = await Water.aggregate(query);
+   console.log(result)
+   // res.json({date, dailyPortions: result});
+   res.json(result);
 }
+// db.books.aggregate([
+//    // First Stage
+//    {     $group : { _id : "$author", books: { $push: "$$ROOT" } }   },
+//    // Second Stage
+//    {     $addFields: { totalCopies : { $sum: "$books.copies" } } }
+//  ])
 
 const getMonth = async (req, res)=>{
    const {_id: user} = req.user;
@@ -51,7 +72,7 @@ const getMonth = async (req, res)=>{
       { $match: {user, date: {$regex : month}} },
       { $group: { _id: "$date", daily: {$sum: "$water"}, doses: {$count: {}}} },
       { $replaceWith: { date: "$_id", percentage: { $round : { $divide: [ "$daily", norma]}}, doses: "$doses" } },
-      {  $sort: {date: 1} }
+      { $sort: {date: 1} }
    ];
    const result = await Water.aggregate(query);
    res.json({month: result, waterNorma: req.user.waterNorma});
@@ -81,12 +102,20 @@ const generatePeriod = async (req, res, next)=>{
       const date = today.toISOString().slice(0, 10);
       const dozen = Math.round(Math.random() * 17 + 0);
       for (let doze = 0; doze < dozen; doze++) {
-         const time = `${Math.round(Math.random() * 16 + 5)}:${Math.trunc(Math.random() * 12) * 5}`;
+         const time = `${Math.round(Math.random() * 16 + 5).toString().padStart(2, 0)}:${(Math.trunc(Math.random() * 12) * 5).toString().padStart(2, 0)}`;
          const water = Math.trunc(Math.random() * 10 + 1) * 50;
          const result = await Water.create({date, time, water, user: req.user._id});
       }
    }
    res.status(201).json("Successfuly created");
+}
+
+const deleteAll = async (req, res, next)=>{
+   const result = await Water.deleteMany();
+   if (!result){
+      next(new HttpError(404, "X3 what wrong"))
+   }
+   res.json(result)
 }
 
 export default {
@@ -97,6 +126,7 @@ export default {
    deleteDoze: controlWrapper(deleteDoze),
    generateMonth: controlWrapper(generateMonth),
    generatePeriod: controlWrapper(generatePeriod),
+   deleteAll: controlWrapper(deleteAll),
 }
 
 
